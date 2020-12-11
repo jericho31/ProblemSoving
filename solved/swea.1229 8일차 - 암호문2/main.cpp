7746705d2c0ac070b2cf53d916cfe2d0db35ca46
jericho31 <dldydwp76@gmail.com>
2020-12-12 00:39:37 +0900

    swea.1229 8일차 - 암호문2
    21min

---
+++ solved/swea.1229 8일차 - 암호문2/main.cpp
@@ -0,0 +1,59 @@
+//21min
+
+#include <iostream>
+#include <list>
+
+using std::cin; using std::cout;
+
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	for (int citer = 1; citer <= 10; ++citer)
+	{
+		int n;
+		cin >> n;
+		std::list<int> ll;
+		for (int i = 0; i < n; ++i) {
+			int tmp;
+			cin >> tmp;
+			ll.push_back(tmp);
+		}
+		cin >> n;
+		for (int i = 0; i < n; ++i) {
+			char c;
+			cin >> c;
+			switch (c)
+			{
+			case 'I': {
+				int x, y;
+				cin >> x >> y;
+				auto iter = ll.begin();
+				while (x--) ++iter;
+				while (y--) {
+					int tmp;
+					cin >> tmp;
+					ll.insert(iter, tmp);
+				}
+			} break;
+			case 'D': {
+				int x, y;
+				cin >> x >> y;
+				auto iter = ll.begin();
+				while (x--) ++iter;
+				while (y--) {
+					ll.erase(iter--);
+					++iter;
+				}
+			} break;
+			}
+		}
+		auto iter = ll.begin();
+		cout << '#' << citer;
+		for (int i = 0; i < 10; ++i) {
+			cout << ' ' << *iter++;
+		}
+		cout << '\n';
+	}
+}
